a498561a43c99baa07eafaf523c474a29539e78b
Andres <jdelcidc5@miumg.edu.gt>
2025-10-15 21:20:47 -0600

    db actualizada con puerto

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,10 +4,11 @@ import mysql from 'mysql2/promise';
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT, // necesario para hacerlo funcionar con Aiven
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: true, //  obligatorio para Aiven
+    rejectUnauthorized: true, // obligatorio para Aiven
   },
   waitForConnections: true,
   connectionLimit: 10
